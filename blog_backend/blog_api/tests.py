b76aedb7420759b85dbe83777d1ae22ecd39f763
Daniel John <dj71286@gmail.com>
2024-05-02 13:08:53 +0100

    Add comprehensive tests for blog_backend

--- blog_backend/blog_api/tests.py
+++ blog_backend/blog_api/tests.py
@@ -1,3 +1,216 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from .models import Blog, Category, Tag, Comment
+from user_api.models import CustomUser
+from .serializers import (
+    CreateUpdateBlogSerializer,
+    BlogSerializer,
+    CreateCommentSerializer,
+    TagSerializer,
+    CategorySerializer,
+    CommentSerializer,
+)
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TestSerializers(TestCase):
+    def setUp(self):
+        # Create a CustomUser instance
+        self.user = CustomUser.objects.create_user(first_name="John",
+                                             last_name="Doe",
+                                             username="test_user",
+                                             email="test@example.com",
+                                             password="password")
+
+        self.category = Category.objects.create(name="Test Category")
+        self.tag1 = Tag.objects.create(name="Test Tag 1")
+        self.tag2 = Tag.objects.create(name="Test Tag 2")
+
+        # Create a Blog instance
+        self.blog = Blog.objects.create(
+            author=self.user,
+            title="Test Blog",
+            content="This is a test blog content",
+            category=self.category,
+        )
+        self.blog.tags.add(self.tag1, self.tag2)
+
+        # Use the created Blog instance's ID for creating the comment
+        self.comment_data = {
+            "author": self.user.id,
+            "blog": self.blog.id,
+            "content": "This is a test comment",
+        }
+
+    def test_create_update_blog_serializer(self):
+        # Define blog_data
+        blog_data = {
+            "author": self.user.id,
+            "title": "Test Blog",
+            "content": "This is a test blog content",
+            "category": self.category.name,
+            "tags": "#TestTag1 #TestTag2",
+        }
+        serializer = CreateUpdateBlogSerializer(data=blog_data)
+        self.assertTrue(serializer.is_valid())
+        serializer = CreateUpdateBlogSerializer(data=blog_data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_create_comment_serializer(self):
+        serializer = CreateCommentSerializer(data=self.comment_data)
+        self.assertTrue(serializer.is_valid())
+
+    def test_blog_serializer(self):
+        blog = Blog.objects.create(
+            author_id=1,  # Assuming author ID
+            title="Test Blog",
+            content="This is a test blog content",
+            category=self.category,
+        )
+        blog.tags.add(self.tag1, self.tag2)
+        serializer = BlogSerializer(blog)
+        expected_data = {
+            "id": blog.id,
+            "author": 1,  # Assuming author ID
+            "title": "Test Blog",
+            "content": "This is a test blog content",
+            "category": self.category.id,
+            "tags": [self.tag1.id, self.tag2.id],
+            "created_at": blog.created_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            "updated_at": blog.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_category_serializer(self):
+        serializer = CategorySerializer(self.category)
+        expected_data = {
+            "id": self.category.id,
+            "name": "Test Category",
+            "created_at": self.category.created_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            "updated_at": self.category.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_tag_serializer(self):
+        serializer = TagSerializer(self.tag1)
+        expected_data = {
+            "id": self.tag1.id,
+            "name": "Test Tag 1",
+            "created_at": self.tag1.created_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            "updated_at": self.tag1.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+    def test_comment_serializer(self):
+        comment = Comment.objects.create(
+            blog_id=1,  # Assuming blog ID
+            author_id=1,  # Assuming author ID
+            content="This is a test comment",
+        )
+        serializer = CommentSerializer(comment)
+        expected_data = {
+            "id": comment.id,
+            "author": 1,  # Assuming author ID
+            "content": "This is a test comment",
+            "created_at": comment.created_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            "updated_at": comment.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = CustomUser.objects.create_user(
+            first_name="John",
+            last_name="Doe",
+            username="test_user",
+            email="test@example.com",
+            password="password"
+        )
+        self.category = Category.objects.create(name="Test Category")
+        self.tag1 = Tag.objects.create(name="Test Tag 1")
+        self.tag2 = Tag.objects.create(name="Test Tag 2")
+        self.blog_data = {
+            "title": "Test Blog",
+            "content": "This is a test blog content",
+            "category": self.category.name,
+            "tags": "#TestTag1 #TestTag2",
+        }
+        self.client.force_login(self.user)
+
+    def test_create_blog(self):
+        url = reverse("create")
+        response = self.client.post(url, self.blog_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_blogs(self):
+        url = reverse("allblogs")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_blog(self):
+        category, created = Category.objects.get_or_create(name="Test Category")
+        blog = Blog.objects.create(
+            author=self.user,
+            title="Test Blog",
+            content="This is a test blog content",
+            category=category,
+        )
+        url = reverse("blog", kwargs={"pk": blog.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create(username="test_user", email="test@example.com", password="password")
+        self.category = Category.objects.create(name="Test Category")
+        self.tag1 = Tag.objects.create(name="Tag1")
+        self.tag2 = Tag.objects.create(name="Tag2")
+        self.blog = Blog.objects.create(
+            author=self.user,
+            title="Test Blog",
+            content="This is a test blog content",
+            category=self.category
+        )
+        self.comment = Comment.objects.create(
+            blog=self.blog,
+            author=self.user,
+            content="This is a test comment"
+        )
+
+    def test_category_str(self):
+        self.assertEqual(str(self.category), "Test Category")
+
+    def test_tag_str(self):
+        self.assertEqual(str(self.tag1), "Tag1")
+        self.assertEqual(str(self.tag2), "Tag2")
+
+    def test_blog_str(self):
+        self.assertEqual(str(self.blog), "Test Blog")
+
+    def test_comment_str(self):
+        expected_str = f"Comment by {self.user.username} on {self.blog.title}"
+        self.assertEqual(str(self.comment), expected_str)
+
+    def test_blog_created_at(self):
+        self.assertIsNotNone(self.blog.created_at)
+        self.assertTrue(timezone.now() - self.blog.created_at < timezone.timedelta(seconds=1))
+
+    def test_blog_updated_at(self):
+        self.assertIsNotNone(self.blog.updated_at)
+        self.assertTrue(timezone.now() - self.blog.updated_at < timezone.timedelta(seconds=1))
+
+    def test_comment_created_at(self):
+        self.assertIsNotNone(self.comment.created_at)
+        self.assertTrue(timezone.now() - self.comment.created_at < timezone.timedelta(seconds=1))
+
+    def test_comment_updated_at(self):
+        self.assertIsNotNone(self.comment.updated_at)
+        self.assertTrue(timezone.now() - self.comment.updated_at < timezone.timedelta(seconds=1))
--- blog_backend/user_api/tests.py
+++ blog_backend/user_api/tests.py
@@ -1,3 +1,154 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework import status
+from datetime import datetime
+from django.core.exceptions import ValidationError
+from user_api.serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 
-# Create your tests here.
+UserModel = get_user_model()
+
+class TestUserSerializers(TestCase):
+    def setUp(self):
+        self.user_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'john.doe@example.com',
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        self.user = UserModel.objects.create_user(**self.user_data)
+
+    def test_user_register_serializer_create(self):
+        get_user_model().objects.all().delete()
+
+        serializer = UserRegisterSerializer(data=self.user_data)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        user_obj = serializer.save()
+        self.assertEqual(user_obj.email, self.user_data['email'])
+        self.assertEqual(user_obj.first_name, self.user_data['first_name'])
+        self.assertEqual(user_obj.last_name, self.user_data['last_name'])
+        self.assertEqual(user_obj.username, self.user_data['username'])
+        self.assertTrue(user_obj.created_at)
+        self.assertTrue(user_obj.updated_at)
+
+    def test_user_register_serializer_update(self):
+        updated_data = {
+            'password': 'new_password123'
+        }
+        serializer = UserRegisterSerializer(instance=self.user, data=updated_data, partial=True)
+        self.assertTrue(serializer.is_valid())
+        user_obj = serializer.save()
+        self.assertTrue(user_obj.updated_at)
+        self.assertEqual(user_obj.password, updated_data['password'])
+
+    def test_user_login_serializer_valid_data(self):
+        serializer = UserLoginSerializer(data=self.user_data)
+        self.assertTrue(serializer.is_valid())
+        user = serializer.check_user(self.user_data)
+        self.assertEqual(user, self.user)
+
+    def test_user_login_serializer_invalid_data(self):
+        invalid_data = {
+            'email': 'invalid@example.com',
+            'password': 'invalidpassword'
+        }
+        serializer = UserLoginSerializer(data=invalid_data)
+        with self.assertRaises(ValidationError):
+            serializer.check_user(invalid_data)
+
+    def test_user_serializer(self):
+        serializer = UserSerializer(instance=self.user)
+        expected_fields = {'id', 'first_name', 'last_name', 'email', 'username', 'created_at', 'updated_at'}
+        self.assertEqual(set(serializer.data.keys()), expected_fields)
+
+
+class TestUserViews(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'john.doe@example.com',
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        self.user = UserModel.objects.create_user(**self.user_data)
+        self.client.login(email=self.user_data['email'], password=self.user_data['password'])
+
+    def test_user_register_view(self):
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        user_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': f'john.doe.{timestamp}@example.com',
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        url = '/api/register'
+        response = self.client.post(url, user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_login_view(self):
+        url = '/api/login'
+        response = self.client.post(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_logout_view(self):
+        url = '/api/logout'
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_password_reset_view(self):
+        url = f'/api/reset-password/{self.user.id}/'
+        new_password = 'new_password123'
+        data = {'password': new_password}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        # Verify password has been updated
+        updated_user = UserModel.objects.get(id=self.user.id)
+        self.assertEqual(updated_user.password, new_password)
+
+    def test_user_view(self):
+        url = '/api/user'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_by_id_view(self):
+        url = f'/api/user/{self.user.id}/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestCustomUserModel(TestCase):
+    def test_create_user(self):
+        user = UserModel.objects.create_user(
+            email='test@example.com',
+            password='testpassword',
+            first_name='John',
+            last_name='Doe',
+            username='johndoe'
+        )
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertTrue(user.check_password('testpassword'))
+        self.assertEqual(user.first_name, 'John')
+        self.assertEqual(user.last_name, 'Doe')
+        self.assertEqual(user.username, 'johndoe')
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        admin_user = UserModel.objects.create_superuser(
+            email='admin@example.com',
+            password='adminpassword',
+            first_name='Admin',
+            last_name='User',
+            username='adminuser'
+        )
+        self.assertEqual(admin_user.email, 'admin@example.com')
+        self.assertTrue(admin_user.check_password('adminpassword'))
+        self.assertEqual(admin_user.first_name, 'Admin')
+        self.assertEqual(admin_user.last_name, 'User')
+        self.assertEqual(admin_user.username, 'adminuser')
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
